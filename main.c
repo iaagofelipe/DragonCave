c700099ba2f5e6e8c9c8839f635402098752b971
Iago Felipe <iaagofelipe@gmail.com>
2019-10-14 21:39:59 -0300

    criado as structs para guerreiro, mago e arqueiro

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int main(int argc, char const *argv[])
-{
-    
-    return 0;
+
+struct Guerreiro{
+    int id;
+    int vida;
+    int ataque_corpo_corpo;
+    int ataque_distancia;
+    int cura
+}
+struct Arqueiro{
+    int id;
+    int vida;
+    int ataque_corpo_corpo;
+    int ataque_distancia;
+    int cura
+}
+struct Mago{
+    int id;
+    int vida;
+    int magia;
+    int ataque_corpo_corpo;
+    int ataque_distancia;
+    int cura
+}
+
+int main(int argc, char const *argv[]) {
+    printf ("\n");
+    printf ("\n");
+    printf ("Bem vindo ao DragonCave!!\n");
+    printf ("\n");
+    printf ("\n");
+    printf ("Agora preciso que voce escolha uma classe");
+    printf ("\n");
+    printf ("\n");
 }
