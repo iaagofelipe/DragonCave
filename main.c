08df15a7a1b9da46289ef9c58cf0749ff371aa62
Iago Felipe <iaagofelipe@gmail.com>
2019-10-14 23:04:23 -0300

    estabelecido os valores das caracteristicas dos herois

--- main.c
+++ main.c
@@ -43,27 +43,53 @@ struct Mago{
     int nivel;
 };
 
-void personagens();
+void menu()
+void iniciarGuerreiro()
 
 int main(int argc, char const *argv[]) {
-    printf ("\n\nBem vindo ao DragonCave!!\n\n");
-    printf ("Agora preciso que voce escolha uma classe\n\n");
-    printf ("Escolha 1 para Guerreiro\n\n");
-    // scanf ("%d", &1);
-    printf ("Escolha 2 para Arqueiro\n\n");
-
-    printf ("Escolha 3 para Mago\n\n");
-
 
     struct Guerreiro guerreiro;
     struct Arqueiro arqueiro;
     struct Mago mago;
-    guerreiro.id = 1;
-    arqueiro.id = 2;
-    mago.id = 3;
+    iniciarGuerreiro(guerreiro);
+    iniciarArqueiro(arqueiro);
+    iniciarMago(mago);
 }
-void personagen(void){
 
-    
+void menu(){
+    printf ("\n\nBem vindo ao DragonCave!!\n\n");
+    printf ("Agora preciso que voce escolha uma classe\n\n");
+    printf ("Escolha 1 para Guerreiro\n\n");
+    printf ("Escolha 2 para Arqueiro\n\n");
+    printf ("Escolha 3 para Mago\n\n");
 }
+
+void iniciarGuerreiro(struct Guerreiro *guerreiro){
+    guerreiro->pv = 125; 
+    guerreiro->velocidade = 15; 
+    guerreiro->defesa = 30; 
+    guerreiro->forca = 50;
+    guerreiro->xp = 0;
+    guerreiro->nivel = 1;
+}
+void iniciarArqueiro(struct Guerreiro *guerreiro){
+    arqueiro->pv = 100; 
+    arqueiro->velocidade = 30; 
+    arqueiro->defesa = 15; 
+    arqueiro->forca = 30;
+    arqueiro->xp = 0;
+    arqueiro->nivel = 1;
+}
+void iniciarMago(struct Guerreiro *guerreiro){
+    mago->pv = 100; 
+    mago->velocidade = 20; 
+    mago->defesa = 15; 
+    mago->forca = 35;
+    mago->magia = 50;
+    mago->xp = 0;
+    mago->nivel = 1;
+}
+
+
+
 
